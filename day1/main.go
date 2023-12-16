b7f706e94a2623bd1e8c8637b6f3aa44b853abc0
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2023-12-16 14:37:29 +0200

    day one done

--- day1/main.go
+++ day1/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 	"unicode"
 )
 
@@ -18,10 +19,23 @@ func main() {
 			"treb7uchet",
 		}
 	*/
+
+	/*lines := []string{
+		"two1nine",
+		"eightwothree",
+		"abcone2threexyz",
+		"xtwone3four",
+		"4nineeightseven2",
+		"zoneight234",
+		"7pqrstsixteen",
+	}*/
 	lines := readPuzzle(filename)
+	lines = trimPuzzle(lines)
 	lineValues := findLineValues(lines)
 	sum := sumOfLineValues(lineValues)
 
+	//fmt.Println(lines)
+	//fmt.Println(lineValues)
 	fmt.Println(sum)
 }
 
@@ -40,6 +54,40 @@ func readPuzzle(filename string) []string {
 	}
 
 	return lines
+}
+
+func trimPuzzle(lines []string) []string {
+	for i, line := range lines {
+		lines[i] = trimLine(line)
+	}
+	return lines
+}
+
+func trimLine(line string) string {
+
+	line = strings.ReplaceAll(line, "zerone", "01")
+	line = strings.ReplaceAll(line, "oneight", "18")
+	line = strings.ReplaceAll(line, "twone", "21")
+	line = strings.ReplaceAll(line, "threeight", "38")
+	line = strings.ReplaceAll(line, "fiveight", "58")
+	line = strings.ReplaceAll(line, "sevenine", "79")
+	line = strings.ReplaceAll(line, "eightwo", "82")
+	line = strings.ReplaceAll(line, "nineight", "98")
+
+	line = strings.ReplaceAll(line, "one", "1")
+	line = strings.ReplaceAll(line, "two", "2")
+	line = strings.ReplaceAll(line, "six", "6")
+
+	line = strings.ReplaceAll(line, "zero", "0")
+	line = strings.ReplaceAll(line, "four", "4")
+	line = strings.ReplaceAll(line, "five", "5")
+	line = strings.ReplaceAll(line, "nine", "9")
+
+	line = strings.ReplaceAll(line, "three", "3")
+	line = strings.ReplaceAll(line, "seven", "7")
+	line = strings.ReplaceAll(line, "eight", "8")
+
+	return line
 }
 
 func findLineValues(lines []string) []int {
